2dc1f307df925acc6c80bfe2c6df0728aa6ea3be
nietiene <niyomugaboetiene53@gmail.com>
2025-06-20 15:24:42 -0700

    Library managment system

--- Author_Configuration.js
+++ Author_Configuration.js
@@ -2,7 +2,7 @@ const connection = require("./Conn");
 const express = require("express");
 const App = express.Router();
 
-App.post('/Add_Author', (req, res) => {
+App.post('/', (req, res) => {
       const { name, bio } = req.body;
       const sql = "INSERT INTO author(name, bio) VALUES(?, ?)";
       connection.query(sql, [ name, bio ], (err) => {
