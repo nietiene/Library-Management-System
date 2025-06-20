5fcd7151ee8520ab94b501aa3b14cc90346d0ed5
nietiene <niyomugaboetiene53@gmail.com>
2025-06-20 15:34:51 -0700

    Library managment system

--- Main_App.js
+++ Main_App.js
@@ -2,7 +2,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
 const Book_Configuration = require("./Book_Configuration");
-const Autor_Configuration = require("./Author_Configuration");
+const Author_Configuration = require("./Author_Configuration");
 
 require("dotenv").config();
 
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-
     origin:'http://localhost:5173',
 }));
 
@@ -25,7 +24,7 @@ app.use('/Add_New_Book', Book_Configuration); // add new book
 app.use('/Update_Book', Book_Configuration);
 app.use('/Delete_Book', Book_Configuration);
 app.use('/Get_Sigle_Book', Book_Configuration);
-app.use('/Add_Author', Autor_Configuration);
+app.use('/Add_Author', Author_Configuration);
 
 const PORT = process.env.PORT;
 
