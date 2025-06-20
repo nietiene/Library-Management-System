b41fad3dc07cd1cf53540647fb8652a0f1077e2d
nietiene <niyomugaboetiene53@gmail.com>
2025-06-20 10:09:53 -0700

    Library managment system

--- Add_Book.js
+++ Add_Book.js
@@ -1,5 +1,10 @@
-
 const express = require("express");
 const connection = require("./Conn");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/')
