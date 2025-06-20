ed87a16f1d2f6d23bafc15e41760e70eda84fac5
nietiene <niyomugaboetiene53@gmail.com>
2025-06-20 10:16:06 -0700

    Library managment system

--- Main_App.js
+++ Main_App.js
@@ -1,7 +1,20 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+app.use(express.json());
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/homepage', (req, res) => {
+      res.json("Welcome To Library managment system");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${process.env.PORT}`);
+})
