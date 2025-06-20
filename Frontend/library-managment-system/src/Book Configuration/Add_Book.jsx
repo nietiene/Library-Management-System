1be9a9a2c766419d58822b115cf86b4d84d54a4b
nietiene <niyomugaboetiene53@gmail.com>
2025-06-20 11:08:12 -0700

    Library managment system

---
+++ Frontend/library-managment-system/src/Book Configuration/Add_Book.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import axios from "axios"
+
+const Add_New_Book = () => {
+    
+    const { title, setTitle } = useState("");
+    const { isbn, setIsbn } = useState("");
+    const { publisher, setublisher } = useState("");
+    const { publication_year, setPublication_year } = useState("");
+    const { copies_available, setCopies_available } = useState("");
+    const { category, setCategory } = useState("");
+}
