52f716bd2f1fc82bdafd7437e428263d06f7da37
nietiene <niyomugaboetiene53@gmail.com>
2025-06-20 10:19:57 -0700

    Library managment system

--- Add_Book.js
+++ Add_Book.js
@@ -2,4 +2,7 @@ const express = require("express");
 const connection = require("./Conn");
 
 const app = express();
-app.get('/')
+
+app.post('/Add_New_Book', (req, res) => {
+     const { title, isbn, publisher, publication_year, copies_available, category } = req.body;
+})
