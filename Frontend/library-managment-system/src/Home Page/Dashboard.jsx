deefd373ada89d81e197f7d8f1748b076cb81401
nietiene <niyomugaboetiene53@gmail.com>
2025-06-21 16:52:10 -0700

    Library managment system

--- Frontend/library-managment-system/src/Home Page/Dashboard.jsx
+++ Frontend/library-managment-system/src/Home Page/Dashboard.jsx
@@ -1,6 +1,21 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
 
 const Dashboard = () => {
+    
+    const [user, setUser] = useState([]);
+    const[message, setMessage] = useState("");
+    
+    axios.get(`http://localhost:3000/data/user`, {withCredentials: true})
+            .then ((res) => {
+            if (res.data.user) {
+                 navigate("/Dashboard");
+            } else {
+                setMessage(res.data.error);
+            }
+        }).catch ((err) => {
+          setError(err.data.error);
+        });
     return (
         <div>
              <nav>
