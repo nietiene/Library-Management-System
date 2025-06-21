9304b63ba5ba3157767a49c33e51b840500327cc
nietiene <niyomugaboetiene53@gmail.com>
2025-06-21 14:10:41 -0700

    Library managment system

--- Frontend/library-managment-system/src/Member Configuration/Update_Member.jsx
+++ Frontend/library-managment-system/src/Member Configuration/Update_Member.jsx
@@ -9,6 +9,9 @@ const Update_Member = () => {
     const [phone, setPhone] = useState("");
     const [address, setAddress] = useState("");
     const [membership_date, setMembership_date] = useState("");
+    const [error, setError] = useState(null);
+    const [message, setMessage] = useState("");
+
     const member_id = useParams();
 
     useEffect(() => {
@@ -21,8 +24,48 @@ const Update_Member = () => {
                 setAddress(member.address);
                 setAddress(member.address);
                 setMembership_date(member.membership_date);
-             })
+             }).catch((err) => {
+                setError(err.data.error);
+                setTimeout(() => {
+                    setError(null);
+                }, 3000);
+             });
         }
-    })
+
+        handleFetchUser();
+    }, [member_id]);
+
+
+
+return (
+    <div>
+            <form onSubmit={handleAddMember}>
+                <label>Name</label>
+                <input type="text" name="name" 
+                onChange={(e) => setName(e.target.value)}
+                value={name}/> <br />
+
+                <label>Email</label>
+                <input type="email" name="email" 
+                onChange={(e) => setEmail(e.target.value)}/> <br />
+
+                <label>Phone Number</label>
+                <input type="text" name="phone" 
+                onChange={(e) => setPhone(e.target.value)}/> <br />
+
+                <label>Address</label>
+                <input type="text" name="address" 
+                onChange={(e) => setAddress(e.target.value)}/> <br />
+
+                <label>Membership Date</label>
+                <input type="date" name="membership_date" 
+                onChange={(e) => setMembership_date(e.target.value)}/> <br />
+                
+                {error && <p style={{color: 'red'}}>{error}</p>}
+                {message && <p style={{color: 'green'}}>{message}</p>}
+                <button type="submit">Create</button>
+            </form>        
+    </div>
+)
 
 }
