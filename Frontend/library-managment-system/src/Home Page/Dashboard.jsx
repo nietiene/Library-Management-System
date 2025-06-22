b180c30d8a0210a3521d97c53d98f09694bd9e58
nietiene <niyomugaboetiene53@gmail.com>
2025-06-21 17:23:23 -0700

    Library managment system

--- Frontend/library-managment-system/src/Home Page/Dashboard.jsx
+++ Frontend/library-managment-system/src/Home Page/Dashboard.jsx
@@ -17,15 +17,20 @@ const Dashboard = () => {
     }, [])
 
     return (
-        <div>
-             <nav>
+        <div className="bg-gray-700 min-h-screen">
+            <div className="flex justify-center bg-gray-500 p-4 shadow-2xl">
+                <nav className="text-white">
                     <Link to={`/Book_list`}>Books List</Link>
                     <Link to={`/Author_List`}>Authors List</Link>
                     <Link to={`/Member_List`}>Members List</Link>
                     <Link to={`#`}>Logout</Link>
-             </nav>
-            <h2>Welcome {user.username} to our Dashboard</h2>
-            {message && <p style={{color: 'red'}}>{message}</p>}
+                </nav>
+             </div>
+             <div>
+                   <h2 className="">Welcome {user.username} to our Dashboard</h2>
+                   {message && <p style={{color: 'red'}}>{message}</p>}
+             </div>
+
         </div>
     )
 }
