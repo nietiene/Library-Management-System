d2892311d9900a852730c437ca4945ded84f82d8
nietiene <niyomugaboetiene53@gmail.com>
2025-06-21 17:01:28 -0700

    Library managment system

--- Frontend/library-managment-system/src/Home Page/Dashboard.jsx
+++ Frontend/library-managment-system/src/Home Page/Dashboard.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
+import axios from "axios";
 
 const Dashboard = () => {
     
     const [user, setUser] = useState([]);
     const[message, setMessage] = useState("");
 
-    axios.get(`http://localhost:3000/data/user`, {withCredentials: true})
+    useEffect(() => {
+       axios.get(`http://localhost:3000/data/user`, {withCredentials: true})
             .then ((res) => {
               setUser(res.data.user);
-        }).catch ((err) => {
-          setMessage(err.data.error);
+          }).catch ((err) => {
+             setMessage(err.data.error);
         });
+    }, [])
+
     return (
         <div>
              <nav>
@@ -21,7 +25,7 @@ const Dashboard = () => {
                     <Link to={`#`}>Logout</Link>
              </nav>
             <h2>Welcome {user.username} to our Dashboard</h2>
-            {message && <p>{message}</p>}
+            {message && <p style={{color: 'red'}}>{message}</p>}
         </div>
     )
 }
