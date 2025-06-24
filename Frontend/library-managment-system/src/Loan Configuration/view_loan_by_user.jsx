7234ac14ea4f9107850a386cce19b22f8bf301d2
nietiene <niyomugaboetiene53@gmail.com>
2025-06-24 11:20:39 -0700

    Library managment system

--- Frontend/library-managment-system/src/Loan Configuration/view_loan_by_user.jsx
+++ Frontend/library-managment-system/src/Loan Configuration/view_loan_by_user.jsx
@@ -15,7 +15,26 @@ const view_loan = () => {
         }).catch((err) => {
             setMessage(err.data.error);
         })
-    })
+    }, []);
+
+    return (
+        <div>
+            <h2>Your Loans</h2>
+            <table border={2} cellPadding={5}>
+                <tr>
+                    {/* loan_id, book_id, member_id, staff_id, loan_date, return_date, actual_return_date, status */}
+                    <th>Loan Code</th>
+                    <th>Book Code</th>
+                    <th>Book Name</th>
+                    <th>Member Code</th>
+                    <th>Member Name</th>
+                    <th>Loan Date</th>
+                    <th>Return Date</th>
+                    <th>Status</th>
+                </tr>
+            </table>
+        </div>
+    )
 
 }
 
