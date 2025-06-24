a4bc8695d66dc78007857ffd9f2f1f3b865132a7
nietiene <niyomugaboetiene53@gmail.com>
2025-06-24 12:09:40 -0700

    Library managment system

--- Frontend/library-managment-system/src/Loan Configuration/view_loan_by_user.jsx
+++ Frontend/library-managment-system/src/Loan Configuration/view_loan_by_user.jsx
@@ -5,19 +5,22 @@ import { Link, useParams } from "react-router-dom";
 const View_loan = () => {
     const [loan, setLoan] = useState([]);
     const [message, setMessage] = useState("");
-    const { member_id } = useParams();
 
-    axios.get(`http://localhost:3000/member/user`, {withCredentials: true})
-    .then((res) => {
-        if (res.data.memberInfo)
-    })
     useEffect(() => {
+        axios.get(`http://localhost:3000/member/user`, {withCredentials: true})
+             .then((res) => {
+        if (res.data.memberInfo) {
+            const member_id = res.data.memberInfo.member_id;
+        }
+
         axios.get(`http://localhost:3000/loan/view_loan/${member_id}`, {withCredentials: true})
         .then((res) => {
             setLoan(res.data.loans);
         }).catch((err) => {
             setMessage(err.data.error);
         })
+    });
+
     }, [member_id]);
 
     return (
