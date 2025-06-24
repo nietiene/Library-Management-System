9db6d45e11de9d7560b1d32345814a19c4bc7dbc
nietiene <niyomugaboetiene53@gmail.com>
2025-06-24 11:00:25 -0700

    Library managment system

---
+++ Frontend/library-managment-system/src/Loan Configuration/return.jsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { useState } from "react";
+
+
+const Return_book = () => {
+    const [book_id, setBook_id] = useState("");
+    const [message, setMessage] = useState("");
+
+const handleReturn = () => {
+    if (!book_id) {
+        setMessage("Please Enter some book_id");
+        return;
+    }
+        axios.post(`http://localhost:3000/loan/borrow`, 
+            { book_id },
+            { withCredentials: true })
+        .then((res) => {
+            if (res.data.error) {
+                setMessage(res.data.error)
+            } else {
+                  setMessage(res.data.message || "Book borrowed");
+            }
+        }).catch((err) => {
+            console.error(err)
+            setMessage(err.response?.data?.error || "Some thing went wrong"); 
+        })
+
+}   
+
+
+    return (
+        <div>
+            <h3>Borrow a book</h3>
+
+            <input type="text" name="Enter a Book code" 
+            value={book_id} onChange={(e) => setBook_id(e.target.value)}
+            placeholder="Enter book code"
+            /> <br />
+
+            <button onClick={handleBorrow}>Borrow</button>
+
+            {message && <p>{message}</p>}
+        </div>
+    )
+}
+
+export default Borrow_book
