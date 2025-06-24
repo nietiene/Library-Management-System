2b01f8128ed20848a4621b2deea4c087e130c0f0
nietiene <niyomugaboetiene53@gmail.com>
2025-06-23 18:35:48 -0700

    Library managment system

--- Loan_Configuration.js
+++ Loan_Configuration.js
@@ -16,7 +16,25 @@ router.get('/borrow', (req, res) => {
     if (!member_id && !book_id) {
         return res.json({error: "Missing book or book ID"});
     }
-})
+
+    const sql = `SELECT copies_available FROM book WHERE book_id = ?`;
+    connection.query(sql, [book_id], (err, result) => {
+       if (err) return res.json({error: err.message});
+
+       if (result.length === 0 && result[0].copies_available < 1) {
+        return res.json({error: "Book not available"});
+       }
+
+       const loanSql = `INSERT INTO loan (book_id, member_id, staff_id, loan_date, return_date, status)
+                        VALUES(?, ?, ?, ?, ?, ?)`;
+       
+      connection.query(loanSql,
+                     
+      )                  
+    });
+
+
+});
 
 
 module.exports = router;
