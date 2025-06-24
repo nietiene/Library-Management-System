a801b95c02f8a28ef9bdd256fb508229ca0e59f3
nietiene <niyomugaboetiene53@gmail.com>
2025-06-23 20:09:48 -0700

    Library managment system

--- Frontend/library-managment-system/src/Loan Configuration/borrow.jsx
+++ Frontend/library-managment-system/src/Loan Configuration/borrow.jsx
@@ -7,8 +7,6 @@ const Borrow_book = () => {
     const [book_id, setBook_id] = useState("");
     const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        
         axios.post(`http://localhost:3000/loan/borrow`, 
             { book_id },
             { withCredentials: true })
@@ -18,6 +16,15 @@ const Borrow_book = () => {
             console.error(err)
             setMessage(err.data.error); 
         })
-    })
 
+
+    return (
+        <div>
+            <h3>Borrow a book</h3>
+
+            <input type="text" name="Enter a Book code" 
+            value={book_id} onChange={(e) => setBook_id(e.target.value)}
+            /> <br />
+        </div>
+    )
 }
