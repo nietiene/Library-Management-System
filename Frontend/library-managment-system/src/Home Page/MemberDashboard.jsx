dd0701c2561014a384223d0920314b2f65e2318d
nietiene <niyomugaboetiene53@gmail.com>
2025-06-30 09:07:28 -0700

    Library managment system

--- Frontend/library-managment-system/src/Home Page/MemberDashboard.jsx
+++ Frontend/library-managment-system/src/Home Page/MemberDashboard.jsx
@@ -114,7 +114,8 @@ const handleLogout = () => {
                   <div className="flex gap-4">
                       <Link to={`/borrow`} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition">Borrow</Link>
                       <Link to={`/return`} className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700 transition">Return</Link>
-                      <Link to={`/viewLoan`} className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-700 transition">View loans</Link>      
+                      <Link to={`/viewLoan`} className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-700 transition">View loans</Link>   
+                      <button onClick={handleLogout} className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition">Logout</button>   
                  </div>   
                 </div>
                 </>
